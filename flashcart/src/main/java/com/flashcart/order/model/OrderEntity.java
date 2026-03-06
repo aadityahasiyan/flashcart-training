f61e72f98880ab0cbb67eec834ecd2487889201b
aadityahasiyan <aaditya.hasiyan656@gmail.com>
2026-03-07 00:36:10 +0530

    added fields to the orderentity pojo

--- flashcart/src/main/java/com/flashcart/order/model/OrderEntity.java
+++ flashcart/src/main/java/com/flashcart/order/model/OrderEntity.java
@@ -1,4 +1,49 @@
 package com.flashcart.order.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double amount;
+
+    private double discount;
+
+    private String status;
+
+    public OrderEntity() {}
+
+    public OrderEntity(double amount, double discount, String status) {
+        this.amount = amount;
+        this.discount = discount;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
